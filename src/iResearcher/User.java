4ac965e9dcd0ff6e5c240cb71659b6fdf2b86837
arnonalmeida <arnon.inter@gmail.com>
2016-03-15 17:55:26 -0300

    Update User.java
    Classe user

--- src/iResearcher/User.java
+++ src/iResearcher/User.java
@@ -1,5 +1,46 @@
 package iResearcher;
 
-public class User {
+public class User 
+{
+	//Atributos de um usuário
+	private String login;
+	private String password;
+	private String username;
+	
+	public String toString()
+	{
+		return username;
+	}
+	
+	public User(String login, String password, String username)
+	{
+		this.login = login;
+		this.password = password;
+		this.username = username;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 }
